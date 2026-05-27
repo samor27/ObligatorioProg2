26991d8ca3ee11a5fbc06b45b5d85b7cd7013b6c
samor27 <samor1@correo.um.edu.uy>
2026-05-27 15:25:57 -0300

    .

---
+++ obligatorio2026/src/main/resources/Eventos.java
@@ -0,0 +1,9 @@
+package obligatorio2026.src.main.resources;
+
+public class Eventos {
+    private TipoEvento tipoEvento;
+    private String instrucciones;
+    public enum TipoEvento{
+        CPU, RAM, DISC
+    }
+}
---
+++ obligatorio2026/src/main/resources/Proceso.java
@@ -0,0 +1,22 @@
+package obligatorio2026.src.main.resources;
+
+public class Proceso {
+    private int pid;
+    private String nombre;
+    private Usuario propietario;
+    private int prioridad;
+    private TipoEstado tipoEstado;
+    private TipoFinalizacion tipoFinalizacion;
+    private Usuario finalizadoPor;
+    private (hay q poner el tipo de tad) eventos;
+
+
+
+    public enum TipoEstado{
+        NEW, PENDING, RUNNING, FINISHED
+    }
+
+    public enum TipoFinalizacion{
+        OK, ERROR, TERMINATED
+    }
+}
